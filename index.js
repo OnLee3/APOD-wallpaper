8358c02d771510e23962ddf5b4465de134d47516
onLee3 <dlwotjr2@gmail.com>
2023-03-27 22:45:25 +0900

    feat: get API Key from user

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import fs from 'fs/promises';
 import { setWallpaper } from 'wallpaper';
 import dotenv from 'dotenv';
 import cron from 'node-cron';
+import inquirer from 'inquirer';
 
 dotenv.config();
-const APOD_API_KEY = process.env.APOD_API_KEY;
 const APOD_API_URL = 'https://api.nasa.gov/planetary/apod';
 
 async function fetchApodImage() {
   try {
     const response = await axios.get(APOD_API_URL, {
       params: {
-        api_key: APOD_API_KEY,
+        api_key: process.env.APOD_API_KEY,
       },
     });
 
@@ -51,10 +51,57 @@ async function fetchAndSetWallpaper() {
   }
 }
 
-cron.schedule('0 12 * * *', async () => {
+async function getApiKey() {
+  try {
+    const apiKey = await fs.readFile('.env', 'utf-8');
+    if (apiKey.trim().length > 0) {
+      console.log('API key found in .env file');
+      return apiKey.trim();
+    }
+  } catch (error) {
+    // ignore error if file does not exist
+  }
+
+  const questions = [
+    {
+      type: 'input',
+      name: 'apiKey',
+      message: 'Enter your APOD API key:',
+      validate: function (value) {
+        if (value.trim().length === 0) {
+          return 'Please enter your API key.';
+        }
+        return true;
+      },
+    },
+  ];
+
+  const answers = await inquirer.prompt(questions);
+  const apiKey = answers.apiKey.trim();
+
+  try {
+    await fs.writeFile('.env', `APOD_API_KEY=${apiKey}\n`);
+    console.log('API key saved to .env file');
+  } catch (error) {
+    console.error('Error saving API key to .env file:', error);
+  }
+
+  return apiKey;
+}
+
+async function main() {
+  if (!process.env.APOD_API_KEY) {
+    const apiKey = await getApiKey();
+    process.env.APOD_API_KEY = apiKey;
+  }
+
+  cron.schedule('0 12 * * *', async () => {
     console.log('Fetching and setting APOD wallpaper...');
     await fetchAndSetWallpaper();
     console.log('Done.');
-});
+  });
 
-fetchAndSetWallpaper();
+  await fetchAndSetWallpaper();
+}
+
+main();
