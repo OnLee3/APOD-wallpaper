23a58e5090e8e92401c01d4f4cb4061391d973c6
jslee01 <jslee01@miridih.com>
2023-03-27 22:14:09 +0900

    Init

---
+++ index.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import fs from 'fs/promises';
+import { setWallpaper } from 'wallpaper';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const APOD_API_KEY = process.env.APOD_API_KEY;
+const APOD_API_URL = 'https://api.nasa.gov/planetary/apod';
+
+async function fetchApodImage() {
+  try {
+    const response = await axios.get(APOD_API_URL, {
+      params: {
+        api_key: APOD_API_KEY,
+      },
+    });
+
+    const { url, media_type } = response.data;
+
+    if (media_type !== 'image') {
+      console.log('The APOD for today is not an image. Try again tomorrow.');
+      return;
+    }
+
+    return url;
+  } catch (error) {
+    console.error('Error fetching APOD image:', error);
+  }
+}
+
+async function downloadAndSetWallpaper(imageUrl) {
+    try {
+      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+      const imageBuffer = Buffer.from(response.data, 'binary');
+      const imagePath = 'apod.jpg';
+  
+      await fs.writeFile(imagePath, imageBuffer);
+      await setWallpaper(imagePath);
+      console.log('Wallpaper set successfully');
+    } catch (error) {
+      console.error('Error downloading and setting wallpaper:', error);
+    }
+  }  
+
+(async () => {
+  const imageUrl = await fetchApodImage();
+
+  if (imageUrl) {
+    await downloadAndSetWallpaper(imageUrl);
+  }
+})();
