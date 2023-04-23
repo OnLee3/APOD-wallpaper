0180f190e47610d98281a97c158fa4cff121363d
onLee3 <dlwotjr2@gmail.com>
2023-04-23 21:03:50 +0900

    chore: remove console

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ function createWindow() {
     },
   });
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   mainWindow.loadFile(path.join(__dirname, "index.html"));
   mainWindow.on("closed", () => (mainWindow = null));
 }
